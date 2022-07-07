3cfecf281673b0c1313b88fd51961d65b8448023
Kacey Ellis <kaceyme93@gmail.com>
2022-07-07 09:40:38 -0400

    Small changes

--- api/activities.js
+++ api/activities.js
@@ -2,6 +2,9 @@ const express = require('express');
 const activitiesRouter = express.Router();
 const { getAllActivities } = require('../db/activities')
 const { getPublicRoutinesByActivity } = require('../db/routines')
+const { getUserById }= require('../db/users')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = process.env
 
 // GET /api/activities/:activityId/routines
 activitiesRouter.get('/:activityId/routines', async (req, res, next) => {
@@ -31,7 +34,31 @@ activitiesRouter.get('/', async (req, res, next) => {
     )
 })
 // POST /api/activities
+activitiesRouter.post('/', async (req, res, next)  => {
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+    //Check for token
+    if (auth.startsWith(prefix)) {
+        const token = auth.slice(prefix.length)
 
+        try{
+            //jwt verify to get user id
+            const { id } = jwt.verify(token, JWT_SECRET);
+
+            if (id) {
+                const user = await getUserById(id);
+                res.send({
+                    id: user.id,
+                    username: user.username
+                })
+                }
+
+        }catch(error){
+            console.log(error)
+            next(error)
+        }
+    }
+})
 // PATCH /api/activities/:activityId
 
 module.exports = activitiesRouter;
